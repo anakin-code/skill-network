ca1191c7af4e6e11d6f9edf0fcd88f93972b892a
tk-dev <169213966+SkdTknr@users.noreply.github.com>
2026-05-18 06:39:51 +0900

    name change!

--- server/src/main/java/skill/server/repository/SkillNetworkRepository.java
+++ server/src/main/java/skill/server/repository/SkillNetworkRepository.java
@@ -2,7 +2,6 @@ package skill.server.repository;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
-import simplex.bn26.master.server.dto.response.*;
 import skill.server.dto.response.*;
 
 import java.util.List;
